60c364ef93fb6e610aaff1c6377aa06390c91a21
soeun lee <thdmsdl87@gmail.com>
2020-04-08 20:14:10 +0900

    Add Contents component for showing selected content's description and title

---
+++ src/components/Contents.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Contents extends Component{
+    render(){
+        return(
+            <nav>
+                <ul>
+                    <li><a href="1.html">CSS</a></li>
+                    <li><a href="2.html">HTML</a></li>
+                    <li><a href="3.html">JavaScript</a></li>
+                </ul>
+
+            </nav>
+        );
+    }
+}
+
+export default Contents;
