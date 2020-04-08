685b391b9d62980e1233b1e2f5299c4869a58c44
soeun lee <thdmsdl87@gmail.com>
2020-04-08 20:12:13 +0900

    Add Subject component for Main subject

---
+++ src/components/Subject.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Subject extends Component{
+    render(){
+        return(
+            <header>
+                <h1>
+                    <a href="/">WEB</a>
+                </h1>
+                welcome to world wide web!
+            </header>
+        );
+    }
+}
+
+export default Subject;
