14b9c4ca1e424ff50b90b6283464b612b9480cdf
soeun lee <thdmsdl87@gmail.com>
2020-04-08 22:43:45 +0900

    welcome-branch setting error...

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import Toc from './components/Toc';
 import logo from './logo.svg';
 import './App.css';
 
-{/* 초기엔 welcome모드고, web 누르면 하단에 welcome 어쩌구가 뜨게
--> Subject의 title 링크 누르면 mode가 welcome으로 바꾸고 contents component에 반영
--> 이때 새로 페이지를 그릴려면 render를 호출해야하고, 이를 위해선 state값을 바꿔야한다.  */}
 class App extends Component{
   constructor(props){
     super(props);
     this.state={
       mode:'welcome',
-      subject:{title: "web", sub: "world wide web!"},
-      welcome:{title:"welcome", desc:"Hello, React!"},
+      subject:{title: "web", sub: "welcome to world wide web!"},
       contents:[
         {id:1, title:"HTML", desc:"HTML is HyperText Markup Language"},
         {id:2, title:"CSS", desc:"CSS is for design"},
@@ -23,17 +19,12 @@ class App extends Component{
     }
   }
   render(){
-    let _title, _desc, _article=null;
-    if(this.state.mode==='welcome'){
-      _title=this.state.welcome.title;
-      _desc=this.state.welcome.desc;
-    }
     return(
       <div className="App">
         <header className="App-header">
           <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
           <Toc data={this.state.contents}></Toc>
-          <Contents title={_title} desc={_desc}></Contents>
+          <Contents title="HTML" desc="HTML is HyperText Markup Languages"></Contents>
         </header>
 
       </div>
