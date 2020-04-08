28bfe7198e3194ce4112dd6e7cead21d47711601
soeun lee <thdmsdl87@gmail.com>
2020-04-09 01:26:58 +0900

    Add onChangePage function for changing state value

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ import './App.css';
   -> Subject의 title 링크 누르면 (여기에 event 함수설치) mode가 welcome으로 바꾸고 contents component에 반영
 -> 이때 새로 페이지를 그릴려면 render를 호출해야하고, 이를 위해선 state값을 바꿔야한다.  */}
 
+{/* Toc에 있는 title클릭시 mode를 read로 바꾼 후 하단에 해당 desc, title같이 뜨게
+-> 내가 클릭한 id에 해당하는 contente의 title, desc를 보여주면 됨*/}
+
 class App extends Component{
   constructor(props){
     super(props);
     this.state={
       mode:'read',
+      selected_content_id:1,
       subject:{title: "web", sub: "world wide web!"},
       welcome:{title:"welcome", desc:"Hello, React!"},
       contents:[
@@ -22,23 +26,48 @@ class App extends Component{
       ]
     }
   }
+  getReadContents(){
+    for(let i=0;i<this.state.contents.length;i++){
+      let data=this.state.contents[i];
+      if(data.id===this.state.selected_content_id){
+        return data;
+      }
+    }
+  }
   render(){
-    let _title, _desc, _article=null;
+    let _title, _desc, _article, _content=null;
     if(this.state.mode==='welcome'){
       _title=this.state.welcome.title;
       _desc=this.state.welcome.desc;
+      _article=<Contents title={_title} desc={_desc}></Contents>
+    }
+    else if(this.state.mode==='read'){
+     _content=this.getReadContents();
+     console.log(_content);
+     _article=<Contents title={_content.title} desc={_content.desc}></Contents>
     }
     return(
       <div className="App">
         <header className="App-header">
+
           <Subject title={this.state.subject.title} sub={this.state.subject.sub}
             onChangePage={function(e){
               this.setState({
                mode:'welcome'
             });
           }.bind(this)}></Subject>
-          <Toc data={this.state.contents}></Toc>
-          <Contents title={_title} desc={_desc}></Contents>
+
+          <Toc onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+          }.bind(this)}
+          data={this.state.contents}>
+          </Toc>
+
+          {_article}
+
         </header>
 
       </div>
