2da4f22051ca4dc88897f58c4c31f49a425651b0
soeun lee <thdmsdl87@gmail.com>
2020-04-08 22:04:44 +0900

    Fix Toc to automatically make tag by using for-syntax

--- src/components/Toc.js
+++ src/components/Toc.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
+{/* 
+여기서는 App에서 받아온 data props를 그대로 보여주려함. 단 매번 태그 생성하기 어려우니 자동으로 생성해주자.
+for문으로 data하나하나를 list 에 넣어서 push 해주기
+단, 이 경우 key 필요하다.
+*/}
+
 class Toc extends Component{
     render(){
+        let lists=[];
+        const data=this.props.data;
+        for(let i=0;i<data.length;i++){
+            lists.push(
+                <li key={data[i].id}><a href={"}/content/"+data[i].id}>{data[i].title}</a></li>
+            )
+        }            
         return(
             <nav>
                 <ul>
-                    <li><a href="1.html">CSS</a></li>
-                    <li><a href="2.html">HTML</a></li>
-                    <li><a href="3.html">JavaScript</a></li>
+                    {lists}
                 </ul>
 
             </nav>
-        );
+        )
     }
 }
 
