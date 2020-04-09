fb0411a2ae20d8533fe57dd0b52f3610d14bc825
soeun lee <thdmsdl87@gmail.com>
2020-04-09 11:27:54 +0900

    Add onClick event on every tag for changing mode when link clicked

--- src/components/Control.js
+++ src/components/Control.js
@@ -5,9 +5,19 @@ class Control extends Component{
         return(
             <header>
                <ul>
-                   <li><a href="#">create</a></li>
-                   <li><a href="#">update</a></li>
-                   <li><input type="button" value="delete"></input></li>              
+                   <li><a href="#" onClick={function(e){
+                      e.preventDefault();
+                      this.props.onChangeMode('create');
+                      }.bind(this)}>create</a></li>
+                   <li><a href="#" onClick={function(e){
+                      e.preventDefault();
+                      this.props.onChangeMode('update');
+                      }.bind(this)}>update</a></li>
+                   <li><input type="button" value="delete" 
+                      onClick={function(e){
+                      e.preventDefault();
+                      this.props.onChangeMode('delete');
+                      }.bind(this)}></input></li>              
                 </ul>
             </header>
         );
